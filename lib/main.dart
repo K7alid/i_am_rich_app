4e7dd89bf8a15b8ed2427f8a2a436bbe429ef317
K7alid <khaledahmedebrzhim@gmail.com>
2022-09-07 22:29:07 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I Am Rich'),
+        ),
+        body: Center(
+          child: Image.asset('assets/diamond.png'),
+        ),
+      ),
+    );
+  }
+}
